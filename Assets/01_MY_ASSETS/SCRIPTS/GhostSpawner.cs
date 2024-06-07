2388658bb418173bee62f936fa2cd49646131313
Jonas Wolter <jonas.wolter.ch@gmail.com>
2024-06-07 16:45:31 +0200

    Work in progress (non working)

---
+++ Assets/01_MY_ASSETS/SCRIPTS/GhostSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSpawner : MonoBehaviour
+{   
+
+    public GameObject ghostPrefab;
+
+
+    // spawn a threedimensional grid of thousands of ghosts
+    void SpawnGhosts()
+    {
+        for (int x = -20; x < 20; x +=3)
+        {
+            for (int y = -20; y < 20; y+=3)
+            {
+                for (int z = -20; z < 20; z+=3)
+                {   
+                    
+
+                    
+                    // instantiate each ghost a random x, y, z offset in a specified range
+                    GameObject ghost = Instantiate(ghostPrefab, new Vector3(x + Random.Range(-1, 1), y + Random.Range(-1, 1), z + Random.Range(-1, 1)), Quaternion.identity);
+
+                    // GameObject ghost = Instantiate(ghostPrefab, new Vector3(x, y, z), Quaternion.identity);
+                    ghost.transform.parent = transform;
+                }
+            }
+        }
+    }
+    // call spawn ghosts when the game starts
+    void Awake()
+    {
+        SpawnGhosts();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
